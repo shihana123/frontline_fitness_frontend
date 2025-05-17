af5cce24ebbb465da094e2366e7561f8bb5848ab
shihana123 <74196835+shihana123@users.noreply.github.com>
2025-05-17 13:51:41 +0530

    Initial commit

---
+++ src/routes/routes.js
@@ -0,0 +1,81 @@
+import DashboardLayout from '@/views/Layout/DashboardLayout.vue';
+import AuthLayout from '@/views/Pages/AuthLayout.vue';
+import Router from 'vue-router';
+
+import NotFound from '@/views/NotFoundPage.vue';
+
+const routes = [
+  {
+    path: '/',
+    redirect: 'login',
+    component: AuthLayout,
+    children: [
+      {
+        path: '/login',
+        name: 'login',
+        component: () => import(/* webpackChunkName: "demo" */ '../views/Pages/Login.vue')
+      },
+      {
+        path: '/register',
+        name: 'register',
+        component: () => import(/* webpackChunkName: "demo" */ '../views/Pages/Register.vue')
+      },
+      // { path: '*', component: NotFound }
+    ]
+  },
+  {
+    path: '/',
+    redirect: 'dashboard',
+    component: DashboardLayout,
+    children: [
+      {
+        path: '/dashboard',
+        name: 'dashboard',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "demo" */ '../views/Dashboard.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/icons',
+        name: 'icons',
+        component: () => import(/* webpackChunkName: "demo" */ '../views/Icons.vue')
+      },
+      {
+        path: '/profile',
+        name: 'profile',
+        component: () => import(/* webpackChunkName: "demo" */ '../views/Pages/UserProfile.vue')
+      },
+      {
+        path: '/maps',
+        name: 'maps',
+        component: () => import(/* webpackChunkName: "demo" */ '../views/GoogleMaps.vue')
+      },
+      {
+        path: '/tables',
+        name: 'tables',
+        component: () => import(/* webpackChunkName: "demo" */ '../views/RegularTables.vue')
+      }
+    ]
+  },
+  
+];
+
+const router = new Router({
+  mode: 'history',
+  routes
+});
+
+// 🔐 Global Guard
+router.beforeEach((to, from, next) => {
+  
+  const isLoggedIn = !!localStorage.getItem('token');
+  if (to.matched.some(record => record.meta.requiresAuth) && !isLoggedIn) {
+    next('/login');
+  } else {
+    next();
+  }
+});
+
+export default routes;
---
+++ src/routes/starterRouter.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import AuthLayout from '@/views/Pages/AuthLayout.vue';
+import Starter from '../views/Pages/Login.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      redirect: 'login',
+      component: AuthLayout,
+      children: [
+        {
+          path: '/login',
+          name: 'login',
+          components: { default: Starter }
+        }
+      ]
+    }
+  ],
+  scrollBehavior: (to, from ,savedPosition) => {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    return { x: 0, y: 0 };
+  }
+});
